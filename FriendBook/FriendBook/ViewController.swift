d63c793cbfb340ed36893416b1cbc32d7f0694e1
Arthurisme <is@live.ca>
2015-12-01 20:46:53 -0500

    FriendBook

--- FriendBook/FriendBook/ViewController.swift
+++ FriendBook/FriendBook/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource,UITableViewDelegate{
+    @IBOutlet weak var TableView: UITableView!
+    
+    let myFriends = ["1","2","3","4","5","6","7",]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.TableView.dataSource = self
+        self.TableView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+return myFriends.count
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = UIColor.greenColor()
+        cell.textLabel!.text = myFriends[indexPath.row]
+        
+        return cell
+        
+    }
 
 }
 
